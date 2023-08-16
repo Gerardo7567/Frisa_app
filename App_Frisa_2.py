a8c3cb7cad9f81b9a04c3d858350c8cb97691bd1
Gerardo7567 <A00828760@itesm.mx>
2023-08-15 19:01:56 -0600

    Actualizacion app

--- App_Frisa_2.py
+++ App_Frisa_2.py
@@ -38,7 +38,8 @@ if add_data:
     #cada variable nueva con la columna donde ira
     new_data = pd.DataFrame({'Nombre': user_name,"Apellido paterno":user_flastname,"Apellido materno":user_slastname,
                 "Correo Electronico":user_mail,"Telefono":int(user_phone),"Convocatoria":user_type})
-    df = pd.concat([new_data,df.loc[:]]).reset_index(drop=True)
+    df = df.append(pd.DataFrame([new_data],columns=df.columns))
+    #df = pd.concat([new_data,df.loc[:]]).reset_index(drop=True)
     #df = df.append(new_data, ignore_index=True)
     #Nombre del archivo dentro del GitHub para actualizarlo
     df.to_csv('Prueba_de_datos.csv',index=False)
